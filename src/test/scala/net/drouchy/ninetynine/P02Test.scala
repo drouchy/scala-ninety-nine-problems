884862fb092c99e39d4ff2e208fae946fd0a2700
David Rouchy <drouchy@gmail.com>
2014-01-01 13:35:27 +0000

    fix some typos

--- src/test/scala/net/drouchy/ninetynine/P02Test.scala
+++ src/test/scala/net/drouchy/ninetynine/P02Test.scala
@@ -9,13 +9,13 @@ class P02Test extends FunSpec with Matchers {
 
   describe("ListUtils") {
     describe("penultimate") {
-      it("Finds the last element of a list") {
+      it("Find the last but one element of a list") {
         penultimate(List(1, 1, 2, 3, 5, 8)) should equal(5)
       }
     }
 
-    describe("lastRecursive") {
-      it("Finds the last element of a list") {
+    describe("penultimateRecursive") {
+      it("Find the last but one element of a list") {
         penultimateRecursive(List(1, 1, 2, 3, 5, 8)) should equal(5)
       }
     }
